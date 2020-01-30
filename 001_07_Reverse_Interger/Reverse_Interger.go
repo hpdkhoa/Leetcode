fd8f15ae75e3c758a2744429cc605d57f7837aa0
hpdkhoa <hpdkhoa2311@gmail.com>
2020-01-31 00:32:18 +0700

    09 for day 1

---
+++ 001_07_Reverse_Interger/Reverse_Interger.go
@@ -0,0 +1,19 @@
+package _01_07_Reverse_Interger
+import "math"
+
+func reverse(x int) int {
+	result := 0
+	for{
+		if v:= x/10; v != 0{
+			result = result * 10 + x % 10
+			x = v
+		} else{
+			result = result * 10 + x
+			if result <= int(math.Pow(2,31)) - 1 && result >= -int(math.Pow(2,31)){
+				return result
+			} else{
+				return 0
+			}
+		}
+	}
+}
---
+++ 002_09_Palindrome_Number/Palindrome_Number.go
@@ -0,0 +1,23 @@
+package _02_09_Palindrome_Number
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false;
+	}
+	temp := x;
+	reverse := 0;
+	for temp > 0{
+		reverse = reverse * 10 + temp % 10;
+		temp /= 10;
+	}
+	temp =x;
+	for reverse != 0 {
+		if reverse % 10 == temp % 10 {
+			reverse /= 10;
+			temp /= 10;
+		} else {
+			return false;
+		}
+	}
+	return true;
+}
