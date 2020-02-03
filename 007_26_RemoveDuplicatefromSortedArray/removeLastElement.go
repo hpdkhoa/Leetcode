dc93c8c812683c135816a8b152eb070a13fa428b
hpdkhoa <hpdkhoa2311@gmail.com>
2020-02-04 01:06:00 +0700

    Day 5: 26 - why the rule is not care about the last element removal

---
+++ 007_26_RemoveDuplicatefromSortedArray/removeLastElement.go
@@ -0,0 +1,15 @@
+package _07_26_RemoveDuplicatefromSortedArray
+
+func removeDuplicates(nums []int) int {
+	for i := 0; i < len(nums) - 1; i++ {
+		if nums[i] == nums[i+1]{
+			for j := i; j < len(nums) - 1; j++{
+				nums[j] = nums[j+1]
+			}
+			nums[len(nums)-1] = 0
+			nums = nums[:len(nums)-1]
+			i--
+		}
+	}
+	return len(nums)
+}
